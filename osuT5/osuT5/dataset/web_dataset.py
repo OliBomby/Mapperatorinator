5dcf3f61047b301ae807b40899ff505f023a3e99
OliBomby <olivierschipper@gmail.com>
2026-03-30 22:46:44 +0200

    let test dataset not be streaming

--- osuT5/osuT5/dataset/web_dataset.py
+++ osuT5/osuT5/dataset/web_dataset.py
@@ -90,12 +90,14 @@ class WebDataset(SequenceDatasetMixin, IterableDataset):
             raise ValueError("Web dataset does not support dt_augment_prob > 0")
 
     def __iter__(self):
-        dataset = load_dataset(self.repo_id, data_files=self.files_split, streaming=True, split="train")
-        dataset = dataset.cast_column("opus", Audio(sampling_rate=self.args.sample_rate, num_channels=1))
-
-        if not self.test:
+        if self.test:
+            dataset = load_dataset(self.repo_id, data_files=self.files_split, streaming=False, split="train")
+        else:
+            dataset = load_dataset(self.repo_id, data_files=self.files_split, streaming=True, split="train")
             dataset = dataset.shuffle(seed=42, buffer_size=100)
             dataset.set_epoch(self.shared.current_epoch)
+
+        dataset = dataset.cast_column("opus", Audio(sampling_rate=self.args.sample_rate, num_channels=1))
 
         return self._iter_rows(dataset)
 
